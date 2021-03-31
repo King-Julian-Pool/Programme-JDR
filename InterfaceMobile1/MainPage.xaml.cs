57a2aa0084e1592da9e369c6c7b30348da616226
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2021-03-31 15:33:26 +0200

    Add files via upload

---
+++ InterfaceMobile1/MainPage.xaml.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InterfaceMobile1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // ------------- Définition des variables ------------- 
+        int PVmax = 200;
+        int PVactuels = 200;
+        int Bouclier = 10;
+        int Armure = 10;
+        int ResistMagique = 10;
+        int Force = 16;
+        int Agilité = 16;
+        int Initiative = 20;
+        int Pm = 5;
+        int degatsInfliges;
+
+        // ------------- Définition des fonctions ------------- 
+        void couleurBarrePV()
+        {
+            if (PVactuels <= PVmax / 4)
+            {
+                ProgressBarPv.ProgressColor = Color.Red;
+            }
+            else if (PVactuels <= PVmax / 2)
+            {
+                ProgressBarPv.ProgressColor = Color.Orange;
+            }
+            else
+            {
+                ProgressBarPv.ProgressColor = Color.Green;
+            }
+        }
+
+        private async void AnimProgressBarPv(ProgressBar ProgressBarPv)
+        {
+            await ProgressBarPv.ProgressTo(Math.Round((double)PVactuels / PVmax, 1), 1000, Easing.Linear);
+        }
+
+        void affichageInitial()
+        {
+            LabelPv.Text = "PV : " + PVactuels + "/" + PVmax;
+            couleurBarrePV();
+            AnimProgressBarPv(ProgressBarPv);
+            LabelBouclier.Text = "Bouclier : " + Bouclier;
+            LabelArmure.Text = "Armure : " + Armure;
+            LabelResistMagique.Text = "Résistance Magique : " + ResistMagique;
+
+            LabelForce.Text = "Force : " + Force;
+            LabelAgilite.Text = "Agilité : " + Agilité;
+            LabelInitiative.Text = "Initiative : " + Initiative;
+            LabelPm.Text = "PM : " + Pm;
+        }
+
+        void heal()
+        {
+            string PVsoignes = EntryDegatsRecus.Text;
+            int PVsoignesNum;
+
+            if (int.TryParse(PVsoignes, out PVsoignesNum) == false)
+            {
+                LabelInfo.Text = "Ce n'est pas un nombre. Try again";
+            }
+            else
+            {
+                LabelInfo.Text = "Vous vous soignez de " + PVsoignesNum + " PV";
+            }
+            PVactuels = PVactuels + PVsoignesNum;
+            affichageInitial();
+        }
+
+        void gainBouclier()
+        {
+            string GainBouclier = EntryDegatsRecus.Text;
+            int GainBouclierNum;
+
+            if (int.TryParse(GainBouclier, out GainBouclierNum) == false)
+            {
+                LabelInfo.Text = "Ce n'est pas un nombre. Try again";
+            }
+            else
+            {
+                LabelInfo.Text = "Vous gagnez " + GainBouclierNum + " points de bouclier";
+            }
+            Bouclier = Bouclier + GainBouclierNum;
+            affichageInitial();
+        }
+        void receptionDegats()
+        {
+            string degatsRecus = EntryDegatsRecus.Text;
+            int degatsRecusNum;
+
+            if (int.TryParse(degatsRecus, out degatsRecusNum) == false)
+            {
+                LabelInfo.Text = "Ce n'est pas un nombre. Try again";
+            }
+            else
+            {
+                LabelInfo.Text = "Vous subissez " + degatsRecusNum + " dégâts";
+            }
+
+            if (Bouclier == 0)
+            {
+                PVactuels = PVactuels - degatsRecusNum;
+            }
+            else if (Bouclier >= degatsRecusNum)
+            {
+                Bouclier = Bouclier - degatsRecusNum;
+            }
+            else
+            {
+                degatsRecusNum = degatsRecusNum - Bouclier;
+                Bouclier = Bouclier - Bouclier;
+                PVactuels = PVactuels - degatsRecusNum;
+            }
+            affichageInitial();
+
+            return;
+        }
+
+        int modifStats()
+        {
+            string ModifStats = EntryModifStats.Text;
+            int ModifStatsNum;
+
+            if (int.TryParse(ModifStats, out ModifStatsNum) == false)
+            {
+                LabelInfo.Text = "Ce n'est pas un nombre. Try again";
+            }
+            return ModifStatsNum;
+        }
+
+        void autoAttack()
+        {
+            int degatsArmes = 1 + Force + Agilité;
+            degatsInfliges = degatsArmes; // console.readline() plutôt que degatsArmes car MJ decide ?
+            LabelInfo.Text = "Vous infligez " + degatsInfliges + " dégâts";
+            return;
+        }
+
+        // ------------- Assignation des contrôles ------------- 
+        private void Page_Appearing(object sender, EventArgs e)
+        {
+            affichageInitial();
+        }
+
+
+
+        private void ButtonModifStats_Click(object sender, EventArgs e)
+        {
+            if (PopUpModifStats.IsVisible == false)
+            {
+                PopUpModifStats.IsVisible = true;
+                PopUpDegatsSoins.IsVisible = false;
+            }
+            else
+            {
+                PopUpModifStats.IsVisible = false;
+            }
+        }
+
+
+        private void ButtonArmure_Click(object sender, EventArgs e)
+        {
+            Armure = modifStats();
+            LabelInfo.Text = "Votre armure passe à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonResistMagique_Click(object sender, EventArgs e)
+        {
+            ResistMagique = modifStats();
+            LabelInfo.Text = "Votre résistance magique passe à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonForce_Click(object sender, EventArgs e)
+        {
+            Force = modifStats();
+            LabelInfo.Text = "Votre force passe à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonAgilite_Click(object sender, EventArgs e)
+        {
+            Agilité = modifStats();
+            LabelInfo.Text = "Votre agilité passe à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonInitiative_Click(object sender, EventArgs e)
+        {
+            Initiative = modifStats();
+            LabelInfo.Text = "Votre initiative passe à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonPm_Click(object sender, EventArgs e)
+        {
+            Pm = modifStats();
+            LabelInfo.Text = "Vos PM passent à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonPVactuels_Click(object sender, EventArgs e)
+        {
+            PVactuels = modifStats();
+            LabelInfo.Text = "Vos PV actuels passent à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonPVmax_Click(object sender, EventArgs e)
+        {
+            PVmax = modifStats();
+            LabelInfo.Text = "Vos PV maximum passent à " + modifStats();
+            affichageInitial();
+        }
+
+        private void ButtonBouclier_Click(object sender, EventArgs e)
+        {
+            Bouclier = modifStats();
+            LabelInfo.Text = "Vos points de bouclier passent à " + modifStats();
+            affichageInitial();
+        }
+
+
+
+        private void ButtonDegatsSoins_Click(object sender, EventArgs e)
+        {
+            if (PopUpDegatsSoins.IsVisible == false)
+            {
+                PopUpDegatsSoins.IsVisible = true;
+                PopUpModifStats.IsVisible = false;
+            }
+            else
+            {
+                PopUpDegatsSoins.IsVisible = false;
+            }
+        }
+
+
+        private void ButtonSubirDegats_Click(object sender, EventArgs e)
+        {
+            receptionDegats();
+        }
+
+        private void ButtonHeal_Click(object sender, EventArgs e)
+        {
+            heal();
+        }
+
+        private void ButtonGainBouclier_Click(object sender, EventArgs e)
+        {
+            gainBouclier();
+        }
+
+
+
+        private void ButtonAutoAttack_Click(object sender, EventArgs e)
+        {
+            autoAttack();
+        }
+    }
+}
